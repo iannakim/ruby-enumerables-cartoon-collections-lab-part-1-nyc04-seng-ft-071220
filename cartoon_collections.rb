538fabe1e21e2ccc019ccb80b45ac3984b2d3bea
Hyun Ji  Kim <dear.hyunji@gmail.com>
2020-07-21 19:46:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,6 +9,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
     array.each_with_index {|names, index| array[names]=index}
-    puts "#{index}. #{{names}}"
+    puts "#{index}. #{names}"
 end
 end
